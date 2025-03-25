214a3c496efb9f64680c6e46503ea4c5f60e7696
ssb-dor <dor@ssb.no>
2025-03-25 12:37:21 +0000

    Draft

--- src/ssb_dash_framework/modals/contribution_analysis.py
+++ src/ssb_dash_framework/modals/contribution_analysis.py
@@ -10,39 +10,63 @@ year = 2023
 group = "fylke"
 variables = ["arealtilskudd", "husdyrtilskudd", "beitetilskudd", "fulldyrket"]
 
-a = conn.query(f"SELECT * FROM skjemadata WHERE soeknads_aar = '{year}' AND variable IN {variables}")
+# df = conn.query(f"SELECT * FROM skjemadata WHERE soeknads_aar = '{year}'")#["variable"].unique()
+conn.query(f"SELECT * FROM skjemadata WHERE soeknads_aar = '{year}'")[
+    "variable"
+].unique()
+
+df
+
+df.pivot_table(index="orgnr", columns="variable", values="value", aggfunc="max")[
+    ["arealtilskudd", "husdyrtilskudd"]
+]
+
+a = conn.query(
+    f"SELECT * FROM skjemadata WHERE soeknads_aar = '{year}' AND variable IN {variables}"
+)
 a["value"] = a["value"].fillna(0)
 a["value"] = a["value"].replace("nan", 0)
 a["value"] = a["value"].astype(float)
 a["value"] = a["value"].astype(int)
 
-b = conn.query(f"SELECT * FROM enhetsinfo WHERE soeknads_aar = '{year}' AND variable = 'saksbehandlende_kommune'").assign(fylke = lambda x: x["value"].str[:2])[["soeknads_aar", "orgnr", "fylke"]]
+b = conn.query(
+    f"SELECT * FROM enhetsinfo WHERE soeknads_aar = '{year}' AND variable = 'saksbehandlende_kommune'"
+).assign(fylke=lambda x: x["value"].str[:2])[["soeknads_aar", "orgnr", "fylke"]]
 
-df_t0 =pd.merge(a,b, on = ["orgnr", "soeknads_aar"])
+df_t0 = pd.merge(a, b, on=["orgnr", "soeknads_aar"])
 
-a = conn.query(f"SELECT * FROM skjemadata WHERE soeknads_aar = '{year-1}' AND variable IN {variables}")
+a = conn.query(
+    f"SELECT * FROM skjemadata WHERE soeknads_aar = '{year-1}' AND variable IN {variables}"
+)
 a["value"] = a["value"].fillna(0)
 a["value"] = a["value"].replace("nan", 0)
 a["value"] = a["value"].astype(float)
 a["value"] = a["value"].astype(int)
 
-b = conn.query(f"SELECT * FROM enhetsinfo WHERE soeknads_aar = '{year-1}' AND variable = 'saksbehandlende_kommune'").assign(fylke = lambda x: x["value"].str[:2])[["soeknads_aar", "orgnr", "fylke"]]
+b = conn.query(
+    f"SELECT * FROM enhetsinfo WHERE soeknads_aar = '{year-1}' AND variable = 'saksbehandlende_kommune'"
+).assign(fylke=lambda x: x["value"].str[:2])[["soeknads_aar", "orgnr", "fylke"]]
 
-df_t1=pd.merge(a,b, on = ["orgnr", "soeknads_aar"])
+df_t1 = pd.merge(a, b, on=["orgnr", "soeknads_aar"])
 
 a = df_t0.groupby(["variable", group], as_index=False).agg({"value": "sum"})
 
 b = df_t1.groupby(["variable", group], as_index=False).agg({"value": "sum"})
 
-merged = pd.merge(a,b, on = ["fylke", "variable"], suffixes = [f"_{year}", f"_{year-1}"]).assign(percentage_change=lambda x: (x[f"value_{year}"] - x[f"value_{year-1}"]) / x[f"value_{year-1}"] * 100)
+merged = pd.merge(
+    a, b, on=["fylke", "variable"], suffixes=[f"_{year}", f"_{year-1}"]
+).assign(
+    percentage_change=lambda x: (x[f"value_{year}"] - x[f"value_{year-1}"])
+    / x[f"value_{year-1}"]
+    * 100
+)
 merged
 
 px.bar(
     merged,
     y="variable",
     x="percentage_change",
-    orientation='h',
+    orientation="h",
     color="fylke",
-    title="Bidrag"
+    title="Bidrag",
 )
-
