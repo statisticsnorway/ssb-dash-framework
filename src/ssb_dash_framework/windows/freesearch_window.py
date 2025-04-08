3fd9aa471b51a78e5488f240910b1124c8f0cd4c
Magnus Theodor Engh <90612127+MagnusEngh@users.noreply.github.com>
2025-04-08 09:22:51 +0000

    Nitpick documentation

--- src/ssb_dash_framework/windows/freesearch_window.py
+++ src/ssb_dash_framework/windows/freesearch_window.py
@@ -1,65 +1,73 @@
 import logging
 
-from dash import html
-from dash import callback
+import dash_bootstrap_components as dbc
 from dash import Input
 from dash import Output
 from dash import State
-import dash_bootstrap_components as dbc
-import dash_ag_grid as dag
+from dash import callback
+from dash import html
 
 from ..modules.freesearch import FreeSearch
 from ..utils.functions import sidebar_button
 
 logger = logging.getLogger(__name__)
 
+
 class FreeSearchWindow(FreeSearch):
     def __init__(self, database):
+        """Initialize the FreeSearchWindow class.
+
+        Args:
+            database: The database connection or object used for querying.
+        """
         super().__init__(database)
         self.callbacks()
 
     def layout(self) -> html.Div:
-        """Generate the layout for the freesearch window.
+        """Generate the layout for the FreeSearch window.
 
         Returns:
-            html.Div: A Div element containing the text area for SQL queries,
-                      input for partitions, a button to run the query,
-                      and a Dash AgGrid table for displaying results.
+            html.Div: A Div element containing:
+                - A modal with a title and body for the FreeSearch module layout.
+                - A sidebar button to toggle the modal.
         """
         layout = html.Div(
             [
                 dbc.Modal(
                     [
                         dbc.ModalHeader(dbc.ModalTitle("Freesearch")),
-                        dbc.ModalBody(
-                            self.module_layout
-                        )
+                        dbc.ModalBody(self.module_layout),
                     ],
-                    id = "freesearch-modal",
+                    id="freesearch-modal",
                     size="xl",
-                    fullscreen = "xxl-down",
+                    fullscreen="xxl-down",
                 ),
-                sidebar_button("🔍", "freesearch", "sidebar-freesearch-button")
+                sidebar_button("🔍", "freesearch", "sidebar-freesearch-button"),
             ]
         )
         logger.debug("Generated layout")
         return layout
 
     def callbacks(self):
+        """Define the callbacks for the FreeSearch window.
+
+        This includes a callback to toggle the visibility of the modal window.
+        """
+
         @callback(  # type: ignore[misc]
             Output("freesearch-modal", "is_open"),
             Input("sidebar-freesearch-button", "n_clicks"),
             State("freesearch-modal", "is_open"),
         )
         def freesearch_modal_toggle(n: int, is_open: bool) -> bool:
-            """Toggles the state of the modal window.
+            """Toggle the state of the modal window.
 
             Args:
                 n (int): Number of clicks on the toggle button.
                 is_open (bool): Current state of the modal (open/closed).
 
             Returns:
-                bool: New state of the modal (open/closed).
+                bool: The new state of the modal (open/closed).
             """
             logger.info("Toggle modal")
             if n:
