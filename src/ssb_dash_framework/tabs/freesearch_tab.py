1584327b157936df11166f18c905923332629f2a
Magnus Theodor Engh <90612127+MagnusEngh@users.noreply.github.com>
2025-04-09 13:41:45 +0200

    Nitpick documentation (#32)

--- src/ssb_dash_framework/tabs/freesearch_tab.py
+++ src/ssb_dash_framework/tabs/freesearch_tab.py
@@ -8,11 +8,22 @@ logger = logging.getLogger(__name__)
 
 
 class FreeSearchTab(FreeSearch):
+    """Implementation of the FreeSearch module as a tab in the application.
+
+    This class extends the FreeSearch base class and provides a layout
+    specific to the tab interface.
+    """
+
     def __init__(self, database):
+        """Initialize the FreeSearchTab with a database connection.
+
+        Args:
+            database (object): Database connection or interface used for executing SQL queries.
+        """
         super().__init__(database)
 
     def layout(self) -> html.Div:
-        """Generate the layout for the FrisokTab.
+        """Generate the layout for the FreeSearchTab.
 
         Returns:
             html.Div: A Div element containing the text area for SQL queries,
