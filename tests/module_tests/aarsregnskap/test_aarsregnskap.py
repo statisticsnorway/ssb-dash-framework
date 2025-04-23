cf5d184becc153f3fb6e8e6446961a92d0c91fd6
ssb-dor <dor@ssb.no>
2025-04-23 10:12:41 +0000

    Add to tests and init

--- tests/module_tests/aarsregnskap/test_aarsregnskap.py
+++ tests/module_tests/aarsregnskap/test_aarsregnskap.py
@@ -1,4 +1,6 @@
 def test_import():
     from ssb_dash_framework import Aarsregnskap
+    from ssb_dash_framework import AarsregnskapTab
 
     assert Aarsregnskap is not None
+    assert AarsregnskapTab is not None
