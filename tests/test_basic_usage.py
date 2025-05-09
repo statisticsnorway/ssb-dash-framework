8fd491d562818a5e2f2feebbb9900db13eb41791
Magnus Theodor Engh <90612127+MagnusEngh@users.noreply.github.com>
2025-05-09 16:39:10 +0200

    More mypy fix

--- tests/test_basic_usage.py
+++ tests/test_basic_usage.py
@@ -8,20 +8,22 @@ from ssb_dash_framework import app_setup
 from ssb_dash_framework import main_layout
 
 
-def test_add_option():
+def test_add_option() -> None:
     VariableSelectorOption("foretak")
 
 
-def test_main_layout():
+def test_main_layout() -> None:
     layout = main_layout([], [], [])
     assert isinstance(
         layout, dbc.Container
     ), f"main_layout not returning dbc.Container, returns: {type(layout)}"
 
 
-def test_app_setup():
+def test_app_setup() -> None:
     port = 8070
     service_prefix = os.getenv("JUPYTERHUB_SERVICE_PREFIX", "/")
-    domain = os.getenv("JUPYTERHUB_HTTP_REFERER", None)
+    domain = os.getenv(
+        "JUPYTERHUB_HTTP_REFERER", "localhost"
+    )  # for testing purposes, set localhost if JUPYTERHUB_HTTP_REFERER is not set
     app = app_setup(port, service_prefix, domain, "superhero")
     assert isinstance(app, Dash)
