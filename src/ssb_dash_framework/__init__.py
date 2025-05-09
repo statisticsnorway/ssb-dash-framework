ff426276e9cb3d8e0f597f1d04d21d19960017ae
Magnus Theodor Engh <90612127+MagnusEngh@users.noreply.github.com>
2025-05-09 16:18:45 +0200

    Fixing mypy

--- src/ssb_dash_framework/__init__.py
+++ src/ssb_dash_framework/__init__.py
@@ -1,45 +1,44 @@
 """SSB Dash Framework."""
 
+# TODO remove this import after refactoring into module as tab/window design.
+from .modals import AltinnControlView
+from .modals import AltinnDataCapture
 from .modals import Control
 from .modals import HBMethod
 from .modals import VisualizationBuilder
-
-# from .windows import RateModelWindow
-from .modules import Aarsregnskap
-from .modules import FreeSearch
-
-# from .modules import RateModel
-from .modules import SkjemadataViewer
-from .modules import SkjemapdfViewer
-from .setup import VariableSelectorOption
+from .modules import aarsregnskap
+from .modules import freesearch
+from .modules import skjemadataviewer
+from .modules import skjemapdfviewer
 from .setup import app_setup
 from .setup import main_layout
-from .tabs import AarsregnskapTab
-from .tabs import BofInformation
-from .tabs import EditingTableLong
-from .tabs import FreeSearchTab
-from .tabs import SkjemapdfViewerTab
-from .tabs import Pimemorizer
+from .setup import variableselector
+from .tabs import aarsregnskap_tab
+from .tabs import bofregistry
+from .tabs import freesearch_tab
+from .tabs import generic
+from .tabs import pi_memorizer
 from .windows import FreeSearchWindow
+from .windows import SkjemapdfViewerWindow
 
-aarsregnskap = ["Aarsregnskap", "AarsregnskapTab"]
-
-for_setup = ["app_setup", "main_layout", "VariableSelectorOption"]
-
-# ratemodel = ["RateModel", "RateModelWindow"]
-
-pimemorizer = ["Pimemorizer"]
-
-freesearch = ["FreeSearch", "FreeSearchTab", "FreeSearchWindow"]
-
-hb_method = ["HBMethod"]
-
-# Defines top level if used in wildcard import
 __all__ = [
-    *for_setup,
-    # *ratemodel,
-    *pimemorizer,
-    *freesearch,
-    *hb_method,
-    *aarsregnskap,
+    "AltinnControlView",
+    "AltinnDataCapture",
+    "Control",
+    "FreeSearchWindow",
+    "HBMethod",
+    "SkjemapdfViewerWindow",
+    "VisualizationBuilder",
+    "aarsregnskap",
+    "aarsregnskap_tab",
+    "app_setup",
+    "bofregistry",
+    "freesearch",
+    "freesearch_tab",
+    "generic",
+    "main_layout",
+    "pi_memorizer",
+    "skjemadataviewer",
+    "skjemapdfviewer",
+    "variableselector",
 ]
