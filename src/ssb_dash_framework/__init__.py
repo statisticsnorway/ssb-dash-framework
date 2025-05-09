44c4aae6aaa1419e0baecf3595e3eaf14331f071
Magnus Theodor Engh <90612127+MagnusEngh@users.noreply.github.com>
2025-05-09 16:27:16 +0200

    Fixing mypy

--- src/ssb_dash_framework/__init__.py
+++ src/ssb_dash_framework/__init__.py
@@ -1,44 +1,89 @@
 """SSB Dash Framework."""
 
-# TODO remove this import after refactoring into module as tab/window design.
 from .modals import AltinnControlView
 from .modals import AltinnDataCapture
 from .modals import Control
 from .modals import HBMethod
 from .modals import VisualizationBuilder
-from .modules import aarsregnskap
-from .modules import freesearch
-from .modules import skjemadataviewer
-from .modules import skjemapdfviewer
+
+modals = [  # TODO Remove these after refactoring
+    AltinnControlView,
+    AltinnDataCapture,
+    Control,
+    HBMethod,
+    VisualizationBuilder,
+]
+
+from .modules import Aarsregnskap
+from .modules import FreeSearch
+from .modules import SkjemadataViewer
+from .modules import SkjemapdfViewer
+
+modules = [
+    Aarsregnskap,
+    FreeSearch,
+    SkjemadataViewer,
+    SkjemapdfViewer,
+]
+
+from .setup import VariableSelector
+from .setup import VariableSelectorOption
 from .setup import app_setup
 from .setup import main_layout
-from .setup import variableselector
-from .tabs import aarsregnskap_tab
-from .tabs import bofregistry
-from .tabs import freesearch_tab
-from .tabs import generic
-from .tabs import pi_memorizer
+
+setup = [
+    VariableSelector,
+    VariableSelectorOption,
+    app_setup,
+    main_layout,
+]
+
+from .tabs import AarsregnskapTab
+from .tabs import BofInformation
+from .tabs import EditingTableLong
+from .tabs import FreeSearchTab
+from .tabs import Pimemorizer
+from .tabs import SkjemapdfViewerTab
+
+tabs = [
+    AarsregnskapTab,
+    BofInformation,
+    FreeSearchTab,
+    Pimemorizer,
+    SkjemapdfViewerTab,
+    EditingTableLong,
+]
+
+from .utils import AlertHandler
+from .utils import DebugInspector
+from .utils import _get_kostra_r
+from .utils import create_alert
+from .utils import hb_method
+from .utils import sidebar_button
+from .utils import th_error
+
+utils = [
+    AlertHandler,
+    DebugInspector,
+    hb_method,
+    sidebar_button,
+    th_error,
+    _get_kostra_r,
+]
+
 from .windows import FreeSearchWindow
 from .windows import SkjemapdfViewerWindow
 
+windows = [
+    FreeSearchWindow,
+    SkjemapdfViewerWindow,
+]
+
 __all__ = [
-    "AltinnControlView",
-    "AltinnDataCapture",
-    "Control",
-    "FreeSearchWindow",
-    "HBMethod",
-    "SkjemapdfViewerWindow",
-    "VisualizationBuilder",
-    "aarsregnskap",
-    "aarsregnskap_tab",
-    "app_setup",
-    "bofregistry",
-    "freesearch",
-    "freesearch_tab",
-    "generic",
-    "main_layout",
-    "pi_memorizer",
-    "skjemadataviewer",
-    "skjemapdfviewer",
-    "variableselector",
+    *modals,
+    *modules,
+    *setup,
+    *tabs,
+    *utils,
+    *windows,
 ]
