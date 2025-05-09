5d905bf0d47e47329969f7f1d2b5a67270303ca9
Magnus Theodor Engh <90612127+MagnusEngh@users.noreply.github.com>
2025-05-09 16:03:34 +0200

    Fix mypy

--- src/ssb_dash_framework/utils/__init__.py
+++ src/ssb_dash_framework/utils/__init__.py
@@ -1,21 +1,19 @@
 """Module containing utility and helper functions shared between components in the framework."""
 
-import importlib
-import os
-import pkgutil
-from collections.abc import Iterator
-from typing import Any
-
+from .alert_handler import AlertHandler
+from .alert_handler import create_alert
+from .debugger_modal import DebugInspector
 from .functions import _get_kostra_r
+from .functions import hb_method
+from .functions import sidebar_button
+from .functions import th_error
 
-current_dir = os.path.dirname(__file__)
-__all__: list[str] = []
-
-module_iter: Iterator[tuple[Any, str, bool]] = pkgutil.iter_modules([current_dir])
-
-for _module_finder, module_name, _is_pkg in module_iter:
-    module = importlib.import_module(f".{module_name}", package=__name__)
-    for attr_name in dir(module):
-        if not attr_name.startswith("_"):
-            globals()[attr_name] = getattr(module, attr_name)
-            __all__.append(attr_name)
+__all__ = [
+    "AlertHandler",
+    "DebugInspector",
+    "_get_kostra_r",
+    "create_alert",
+    "hb_method",
+    "sidebar_button",
+    "th_error",
+]
