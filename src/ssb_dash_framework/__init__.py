27661a5efd3cb9a5f510abebd4e5c5008d3e2dd0
Magnus Theodor Engh <90612127+MagnusEngh@users.noreply.github.com>
2025-05-09 16:31:58 +0200

    Fixing mypy

--- src/ssb_dash_framework/__init__.py
+++ src/ssb_dash_framework/__init__.py
@@ -5,55 +5,20 @@ from .modals import AltinnDataCapture
 from .modals import Control
 from .modals import HBMethod
 from .modals import VisualizationBuilder
-
-modals = [  # TODO Remove these after refactoring
-    AltinnControlView,
-    AltinnDataCapture,
-    Control,
-    HBMethod,
-    VisualizationBuilder,
-]
-
 from .modules import Aarsregnskap
 from .modules import FreeSearch
 from .modules import SkjemadataViewer
 from .modules import SkjemapdfViewer
-
-modules = [
-    Aarsregnskap,
-    FreeSearch,
-    SkjemadataViewer,
-    SkjemapdfViewer,
-]
-
 from .setup import VariableSelector
 from .setup import VariableSelectorOption
 from .setup import app_setup
 from .setup import main_layout
-
-setup = [
-    VariableSelector,
-    VariableSelectorOption,
-    app_setup,
-    main_layout,
-]
-
 from .tabs import AarsregnskapTab
 from .tabs import BofInformation
 from .tabs import EditingTableLong
 from .tabs import FreeSearchTab
 from .tabs import Pimemorizer
 from .tabs import SkjemapdfViewerTab
-
-tabs = [
-    AarsregnskapTab,
-    BofInformation,
-    FreeSearchTab,
-    Pimemorizer,
-    SkjemapdfViewerTab,
-    EditingTableLong,
-]
-
 from .utils import AlertHandler
 from .utils import DebugInspector
 from .utils import _get_kostra_r
@@ -61,29 +26,36 @@ from .utils import create_alert
 from .utils import hb_method
 from .utils import sidebar_button
 from .utils import th_error
-
-utils = [
-    AlertHandler,
-    DebugInspector,
-    hb_method,
-    sidebar_button,
-    th_error,
-    _get_kostra_r,
-]
-
 from .windows import FreeSearchWindow
 from .windows import SkjemapdfViewerWindow
 
-windows = [
-    FreeSearchWindow,
-    SkjemapdfViewerWindow,
-]
-
 __all__ = [
-    *modals,
-    *modules,
-    *setup,
-    *tabs,
-    *utils,
-    *windows,
+    "Aarsregnskap",
+    "AarsregnskapTab",
+    "AlertHandler",
+    "AltinnControlView",
+    "AltinnDataCapture",
+    "BofInformation",
+    "Control",
+    "DebugInspector",
+    "EditingTableLong",
+    "FreeSearch",
+    "FreeSearchTab",
+    "FreeSearchWindow",
+    "HBMethod",
+    "Pimemorizer",
+    "SkjemadataViewer",
+    "SkjemapdfViewer",
+    "SkjemapdfViewerTab",
+    "SkjemapdfViewerWindow",
+    "VariableSelector",
+    "VariableSelectorOption",
+    "VisualizationBuilder",
+    "_get_kostra_r",
+    "app_setup",
+    "create_alert",
+    "hb_method",
+    "main_layout",
+    "sidebar_button",
+    "th_error",
 ]
