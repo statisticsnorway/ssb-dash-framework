fc469ff1e5b2054242f220e433eb8253637fc87f
Magnus Theodor Engh <90612127+MagnusEngh@users.noreply.github.com>
2025-07-15 11:04:33 +0200

    Fixes

--- src/ssb_dash_framework/modules/altinn_editor/altinn_editor_primary_table.py
+++ src/ssb_dash_framework/modules/altinn_editor/altinn_editor_primary_table.py
@@ -1,14 +1,15 @@
 import logging
+from typing import Any
 
 import dash_ag_grid as dag
 from dash import callback
 from dash import html
-from dash import no_update
 from dash.dependencies import Input
 from dash.dependencies import Output
 from dash.dependencies import State
 from dash.exceptions import PreventUpdate
 
+from ...setup.variableselector import VariableSelector
 from ...utils.alert_handler import create_alert
 from ...utils.eimerdb_helpers import create_partition_select
 
@@ -16,10 +17,34 @@ logger = logging.getLogger(__name__)
 
 
 class AltinnEditorPrimaryTable:
+    """Module creating the primary editing table for the Altinn Editor.
 
-    def __init__(self, time_units, conn, variable_selector_instance) -> None:
+    Note:
+        Is in a separate class from the view to futureproof, in future is possible to replace or even make customizable.
+    """
+
+    def __init__(
+        self,
+        time_units: list[str],
+        conn: object,
+        variable_selector_instance: VariableSelector,
+    ) -> None:
+        """Initializes the Altinn Editor primary table module.
+
+        Args:
+            time_units (list[str]): List of time units to be used in the module.
+            conn (object): Database connection object that must have a 'query' method.
+            variable_selector_instance (VariableSelector): An instance of VariableSelector for variable selection.
+
+        Raises:
+            TypeError: If variable_selector_instance is not an instance of VariableSelector.
+            AssertionError: If the connection object does not have a 'query' method.
+        """
         self.time_units = time_units
         assert hasattr(conn, "query"), "The database object must have a 'query' method."
+        assert hasattr(
+            conn, "tables"
+        ), "The database object must have a 'tables' attribute."
         self.conn = conn
         if not isinstance(variable_selector_instance, VariableSelector):
             raise TypeError(
@@ -29,7 +54,8 @@ class AltinnEditorPrimaryTable:
         self.module_layout = self._create_layout()
         self.module_callbacks()
 
-    def _create_layout(self):
+    def _create_layout(self) -> html.Div:
+        """Creates the module layout."""
         return html.Div(
             id="altinn-editor-primary-table",
             style={
@@ -54,10 +80,13 @@ class AltinnEditorPrimaryTable:
             ],
         )
 
-    def layout(self):
+    def layout(self) -> html.Div:
+        """Returns the layout of the module."""
         return self.module_layout
 
-    def module_callbacks(self):
+    def module_callbacks(self) -> None:
+        """Defines the callbacks for the module."""
+
         @callback(  # type: ignore[misc]
             Output("altinnedit-table-skjemadata", "rowData"),
             Output("altinnedit-table-skjemadata", "columnDefs"),
@@ -66,7 +95,9 @@ class AltinnEditorPrimaryTable:
             State("altinnedit-skjemaer", "value"),
             self.variable_selector.get_states(),
         )
-        def hovedside_update_altinnskjema(skjemaversjon, tabell, skjema, *args):
+        def hovedside_update_altinnskjema(
+            skjemaversjon: str, tabell: str, skjema: str, *args: Any
+        ) -> tuple[list[dict[str, Any]] | None, list[dict[str, Any]] | None]:
             schema = self.conn.tables[tabell]["schema"]
             columns = {field["name"] for field in schema}
             if "variabel" in columns and "verdi" in columns:
@@ -81,7 +112,7 @@ class AltinnEditorPrimaryTable:
                 or any(arg is None for arg in args)
             ):
                 return None, None
-            if long_format == True:
+            if long_format:
                 try:
                     partition_args = dict(zip(self.time_units, args, strict=False))
                     df = self.conn.query(
@@ -162,10 +193,12 @@ class AltinnEditorPrimaryTable:
             Input("altinnedit-table-skjemadata", "cellClicked"),
             State("altinnedit-table-skjemadata", "rowData"),
         )
-        def select_variabel(click, row_data):
+        def select_variabel(
+            click: dict[str, Any], row_data: list[dict[str, Any]]
+        ) -> str:
             if row_data is None:
-                return no_update
-            return row_data[click["rowIndex"]]["variabel"]
+                raise PreventUpdate
+            return str(row_data[click["rowIndex"]]["variabel"])
 
         @callback(  # type: ignore[misc]
             Output("alert_store", "data", allow_duplicate=True),
@@ -176,7 +209,13 @@ class AltinnEditorPrimaryTable:
             self.variable_selector.get_states(),
             prevent_initial_call=True,
         )
-        def update_table(edited, tabell, skjema, alert_store, *args):
+        def update_table(
+            edited: list[dict[str, dict[str, Any] | Any]],
+            tabell: str,
+            skjema: str,
+            alert_store: list[dict[str, Any]],
+            *args: Any,
+        ) -> list[dict[str, Any]]:
             if edited is None:
                 raise PreventUpdate
             else:
@@ -202,7 +241,7 @@ class AltinnEditorPrimaryTable:
                 else:
                     long_format = False
 
-                if table_editable_dict[edited_column] == True:
+                if table_editable_dict[edited_column] is True:
                     old_value = edited[0]["oldValue"]
                     new_value = edited[0]["value"]
                     row_id = edited[0]["data"]["row_id"]
@@ -220,7 +259,7 @@ class AltinnEditorPrimaryTable:
                                 **partition_args,
                             ),
                         )
-                        if long_format == True:
+                        if long_format:
                             variabel = edited[0]["data"]["variabel"]
                             alert_store = [
                                 create_alert(
@@ -250,4 +289,12 @@ class AltinnEditorPrimaryTable:
                         ]
                     return alert_store
                 else:
-                    return f"Kolonnen {edited_column} kan ikke editeres!"
+                    alert_store = [
+                        create_alert(
+                            f"Kolonnen {edited_column} kan ikke editeres!",
+                            "danger",
+                            ephemeral=True,
+                        ),
+                        *alert_store,
+                    ]
+                    return alert_store
