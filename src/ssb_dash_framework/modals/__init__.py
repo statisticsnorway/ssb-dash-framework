964f7a20a685646ccf74a1f9785b201acec2843c
Magnus Theodor Engh <90612127+MagnusEngh@users.noreply.github.com>
2025-08-13 15:34:07 +0200

    Move aggdist and change import

--- src/ssb_dash_framework/modals/__init__.py
+++ src/ssb_dash_framework/modals/__init__.py
@@ -1,8 +1,5 @@
 """Modals for use in the application."""
 
-from .agg_dist_plotter import AggDistPlotter
-from .agg_dist_plotter import AggDistPlotterTab
-from .agg_dist_plotter import AggDistPlotterWindow
 from .altinn_control_view import AltinnControlView
 from .control import Control
 from .visualizationbuilder import VisualizationBuilder
@@ -10,9 +7,6 @@ from .visualizationbuilder import VisualizationBuilder
 # from .hb_method import HBMethod
 
 __all__ = [
-    "AggDistPlotter",
-    "AggDistPlotterTab",
-    "AggDistPlotterWindow",
     "AltinnControlView",
     "Control",
     #    "HBMethod",
