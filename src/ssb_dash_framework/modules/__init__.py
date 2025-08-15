26290689c23581388454e1f4da2e28fe4c8e796f
Magnus Theodor Engh <90612127+MagnusEngh@users.noreply.github.com>
2025-08-15 12:33:38 +0200

    Map module (#139)
    * Use geopandas to read geography

--- src/ssb_dash_framework/modules/__init__.py
+++ src/ssb_dash_framework/modules/__init__.py
@@ -25,6 +25,9 @@ from .building_blocks import MultiModuleWindow
 from .freesearch import FreeSearch
 from .freesearch import FreeSearchTab
 from .freesearch import FreeSearchWindow
+from .map_display import MapDisplay
+from .map_display import MapDisplayTab
+from .map_display import MapDisplayWindow
 from .skjemapdfviewer import SkjemapdfViewer
 from .skjemapdfviewer import SkjemapdfViewerTab
 from .skjemapdfviewer import SkjemapdfViewerWindow
@@ -52,6 +55,9 @@ __all__ = [
     "FreeSearch",
     "FreeSearchTab",
     "FreeSearchWindow",
+    "MapDisplay",
+    "MapDisplayTab",
+    "MapDisplayWindow",
     "MultiModule",
     "MultiModuleTab",
     "MultiModuleWindow",
