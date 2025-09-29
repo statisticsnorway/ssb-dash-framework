a50ed99e7960d76ffd4164f6cc6487a92fefe98a
Alex <106316255+alexcroz@users.noreply.github.com>
2025-09-29 14:02:26 +0200

    Update tables.py
    see previous commit

--- src/ssb_dash_framework/modules/building_blocks/tables.py
+++ src/ssb_dash_framework/modules/building_blocks/tables.py
@@ -296,11 +296,11 @@ class EditingTableTab(TabImplementation, EditingTable):
         inputs: list[str],
         states: list[str],
         get_data_func: Callable[..., Any],
+        log_filepath: str,
         update_table_func: Callable[..., Any] | None = None,
         output: str | None = None,
         output_varselector_name: str | None = None,
         number_format: str | None = None,
-        log_filepath: str,
         **kwargs: Any,
     ) -> None:
         EditingTable.__init__(
