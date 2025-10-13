252a22bc8be15b77cd1b5e441d2df931d2e7fc26
Magnus Theodor Engh <90612127+MagnusEngh@users.noreply.github.com>
2025-10-13 09:53:30 +0000

    Tester

---
+++ .devcontainer/sqlite/test_devdata.py
@@ -0,0 +1,9 @@
+from sqlalchemy import text
+from ssb_dash_framework import create_database_engine
+
+engine = create_database_engine("sqlite", sqlite_path=".devcontainer/sqlite/mydb.sqlite")
+query = text("SELECT * FROM enheter")
+with engine.connect() as conn:
+    result = conn.execute(query)
+    rows = result.fetchall()
+print(rows)
