51f84d0687ad3ab9c695ad6ec4741e87e3a1b2e5
Alex <106316255+alexcroz@users.noreply.github.com>
2025-10-15 08:34:16 +0200

    add apply_edits

--- src/ssb_dash_framework/__init__.py
+++ src/ssb_dash_framework/__init__.py
@@ -56,6 +56,7 @@ from .utils import create_alert
 from .utils import enable_app_logging
 from .utils import module_validator
 from .utils import sidebar_button
+from .utils import apply_edits
 
 # from .modals import HBMethod
 # from .utils import _get_kostra_r
