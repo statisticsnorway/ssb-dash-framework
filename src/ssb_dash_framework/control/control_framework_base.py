5a931b9d962256bb58c616f2fb33b0cd97eefe3c
ssb-dor <dor@ssb.no>
2025-11-27 11:22:52 +0100

    Doodling

--- src/ssb_dash_framework/control/control_framework_base.py
+++ src/ssb_dash_framework/control/control_framework_base.py
@@ -1,11 +1,13 @@
 import logging
+import warnings
 
 import pandas as pd
 
 logger = logging.getLogger(__name__)
 
 
-def control(kontroll_id, kontrolltype, skildring, **meta):
+
+def control(kontroll_id, kontrolltype, skildring,kontrollvariabel,sorteringsvariabel, **kwargs):
     """
     Decorator used to attach REQUIRED metadata to control_<id> methods.
 
@@ -23,26 +25,25 @@ def control(kontroll_id, kontrolltype, skildring, **meta):
         "kontrollvariabel",# Optional?
         "sorteringsvariabel", # Optional?
     }
+    meta_dict = {
+        "kontroll_id": kontroll_id,
+        "kontrolltype": kontrolltype,
+        "skildring": skildring,
+        "kontrollvariabel": kontrollvariabel,
+        "sorteringsvariabel": sorteringsvariabel,
+    }
 
     # Check for missing required keys
-    missing = required_keys - meta.keys()
-    if missing:
-        raise ValueError(
-            f"Missing required control metadata fields: {', '.join(missing)}"
-        )
-
-    # Warn or block extra keys
-    extra = meta.keys() - required_keys
-    if extra:
-        raise ValueError(
-            f"Unknown metadata keys for control decorator: {', '.join(extra)}"
-        )
+    for required in required_keys:
+        if required not in meta_dict.keys():
+            raise ValueError(f"This definition is missing required field '{required}'.")
 
     def wrapper(func):
-        func._control_meta = meta
+        func._control_meta = meta_dict
         return func
 
     return wrapper
+
 
 class ControlFrameworkBase:  # TODO: Add some common control methods here for easier reuse.
     """Base class for running control checks.
@@ -69,107 +70,97 @@ class ControlFrameworkBase:  # TODO: Add some common control methods here for ea
         3. Based on the results from 'control_updates' it generates an update query where each change in the results, where the result of a control has changed for an observation, is updated in the 'kontrollutslag' table.
         4. The update query is run, and the process is complete.
     """
+    _required_kontroller_columns = [
+        "kontrollid",
+        "kontrolltype",
+        "skildring",
+    ]
+
+    _required_kontrollutslag_columns = [
+        "kontrollid",
+        "ident",
+        "refnr",
+        "utslag",
+    ]
 
     def __init__(
         self,
-        partitions: list[int | str],
-        partitions_skjema: dict[str, int | str],
+        time_units: list[int | str],
+        applies_to_forms: list[str],
         conn: object,
+        partitions: list[int | str] | None = None, # Deprecated name
+        partitions_skjema: dict[str, int | str] | None= None, # Deprecated name
     ) -> None:
         """Initialize the control framework.
 
         Args:
             partitions: Partition to execute controls on.
             partitions_skjema: Partition specification, including skjema.
-            conn: Database connection object with query and insert methods.
+            conn: Database connection object.
 
         Raises:
             AttributeError: If conn lacks 'query' or 'insert' methods.
             ValueError: if no controls are found for chosen partition.
         """
-        if not hasattr(conn, "query"):
-            raise AttributeError("The 'conn' object must have a 'query' method.")
-        if not hasattr(conn, "insert"):
-            raise AttributeError("The 'conn' object must have a 'insert' method.")
-        self.partitions = partitions
-        self.partitions_skjema = partitions_skjema
-        self.conn = conn
-        self.controls = self.conn.query(
-            "SELECT kontrollid FROM kontroller",
-            partition_select=partitions_skjema,
-        )["kontrollid"].tolist()
-        logger.debug(f"Controls found: {self.controls}")
-        if not self.controls:
-            raise ValueError(
-                f"No controls found for partition: {self.partitions_skjema}"
+        if partitions is not None or partitions_skjema is not None:
+            warnings.warn(
+                "The 'partitions' and 'partitions_skjema' parameters are deprecated. "
+                "Use 'time_units' and 'valid_for' instead.",
+                DeprecationWarning,
+                stacklevel=2
             )
+            # Map old to new
+            if time_units is None:
+                time_units = partitions
+            if applies_to_forms is None:
+                # Needs transformation here
+                applies_to_forms = partitions_skjema
+        self.time_units = time_units
+        self.applies_to_forms = applies_to_forms
 
-    def execute_controls(self) -> int:
-        """Executes control checks and updates existing rows in 'kontrollutslag' if needed.
+        self._required_kontroller_columns = [*self.time_units, *ControlFrameworkBase._required_kontroller_columns]
+        self._required_kontrollutslag_columns = [*self.time_units, *ControlFrameworkBase._required_kontrollutslag_columns]
 
-        Returns:
-            int: Number of rows updated.
-        """
-        df_updates = self.control_updates()
-        if len(df_updates) > 0:
-            logger.info(f"{len(df_updates)} rader oppdateres...")
-            self.conn.query(
-                self.generate_update_query(df_updates), self.partitions_skjema
-            )
-            logger.info("Oppdatering fullført!")
-        else:
-            logger.info("Ingen rader å oppdatere")
-        return len(df_updates)
 
-    def control_updates(self) -> pd.DataFrame:
-        """Identifies rows in 'kontrollutslag' where the control output has changed.
-
-        Returns:
-            pd.DataFrame: DataFrame of rows that need to be updated.
-        """
-        df_allerede_kontrollert = self.conn.query(
-            "SELECT kontrollid, ident, refnr, utslag FROM kontrollutslag",
-            self.partitions_skjema,
-        )
-        df = self.run_all_controls()
-        total_merge = df.merge(
-            df_allerede_kontrollert,
-            on=["kontrollid", "ident", "refnr"],
-            how="outer",
-            indicator=True,
-        ).dropna()
-
-        df_endrede = total_merge[total_merge["utslag_x"] != total_merge["utslag_y"]][
-            ["kontrollid", "ident", "refnr", "verdi", "utslag_x"]
-        ].rename(columns={"utslag_x": "utslag"})
-
-        return df_endrede
-
-    def run_all_controls(self) -> pd.DataFrame:
-        """Runs control methods named like 'control_<kontrollid>' where <id> is in self.controls.
-
-        Returns:
-            pd.DataFrame: Combined DataFrame with all control results.
-
-        Raises:
-            TypeError: if 'df' variable to return is not pd.DataFrame.
-        """
-        df_all_results: list[pd.DataFrame] = []
+    def find_control_methods(self):
+        self.controls = []
         for method_name in dir(self):
-            if method_name[8:] in self.controls and method_name.startswith("control_"):
-                if not callable(getattr(self, method_name)):
-                    raise TypeError(
-                        f"Attribute in class '{method_name}' is not callable. Either make it a method or change its name to not start with 'control_'."
-                    )
-                df_all_results.append(self.run_control(method_name))
-            else:
-                logger.debug(f"{method_name} was not called as it failed the if check.")
+            if hasattr(getattr(test, method_name), "_control_meta"):
+                self.controls.append(method_name)
+        if len(self.controls) == 0:
+            raise ValueError("No control methods found.")
+        print(self.controls)
+
+    def register_controls(self):
+        self.find_control_methods()
+
+
+        
+
+    def execute_controls(self) -> None:
+        control_results = self.run_all_controls()
+        print(control_results)
+
+
+    def run_all_controls(self):
+        self.find_control_methods()
+
+        df_all_results: list[pd.DataFrame] = []
+        for method_name in self.controls:
+            logger.debug(f"Running method: {method_name}")
+            if not callable(getattr(self, method_name)):
+                raise TypeError(
+                    f"Attribute in class '{method_name}' is not callable. Either make it a method or change its name to not start with 'control_'."
+                )
+            df_all_results.append(self.run_control(method_name))
         df = pd.concat(df_all_results).reset_index(drop=True)
+
         if not isinstance(df, pd.DataFrame):
             raise TypeError(
                 f"Control results is not a pandas dataframe, is type: {type(df)}"
             )
         return df
+
 
     def run_control(self, control: str) -> pd.DataFrame:
         """Runs a single control.
@@ -188,102 +179,53 @@ class ControlFrameworkBase:  # TODO: Add some common control methods here for ea
             raise TypeError(
                 f"Result from control method is not a pd.dataframe. Received: '{type(results)}'"
             )
-        # Check if any required columns are missing
+        for column in self._required_kontrollutslag_columns:
+            if column not in results.columns:
+                raise ValueError(f"Missing required column '{column}' for result from '{control}'.")
         return results
 
-    def generate_update_query(self, df_updates: pd.DataFrame) -> str:
-        """Generates a SQL UPDATE query for updating rows in 'kontrollutslag'.
+    
+    def insert_new_records(self):
+        ...
+    
+    def update_existing_records(self):
+        ...
 
-        Args:
-            df_updates (pd.DataFrame): DataFrame with updates to apply.
 
-        Returns:
-            str: SQL query string.
-        """
-        update_query = "UPDATE kontrollutslag SET utslag = CASE"
 
-        for _, row in df_updates.iterrows():
-            update_query += (
-                f" WHEN kontrollid = '{row['kontrollid']}' AND "
-                f"refnr = '{row['refnr']}' THEN {row['utslag']}"
-            )
-
-        update_query += " ELSE utslag END"
-        update_query += (
-            " WHERE "
-            + " OR ".join(
-                [
-                    f"(kontrollid = '{row['kontrollid']}' AND refnr = '{row['refnr']}')"
-                    for _, row in df_updates.iterrows()
-                ]
-            )
-            + ";"
+    @control(
+        kontroll_id="standard_1",
+        kontrolltype="info",
+        skildring="Skjemaet har en kommentar",
+        kontrollvariabel=None,
+        sorteringsvariabel=None
+    )
+    def control_comment_exists(self):
+        conn = db.EimerDBInstance(
+        "ssb-dapla-felles-data-produkt-prod",
+        "produksjonstilskudd_altinn3",
         )
+        res = conn.query("SELECT * FROM kontaktinfo WHERE aar = '2018'")
+        return res
 
-        return update_query
 
-    def control_new_rows(self) -> pd.DataFrame:
-        """Identifies new rows that are not already present in 'kontrollutslag'.
 
-        Returns:
-            pd.DataFrame: DataFrame of new rows to insert.
-        """
-        try:
-            query = f"SELECT {', '.join(self.partitions_skjema.keys())}, kontrollid, ident, refnr, utslag FROM kontrollutslag"
-            logger.debug(
-                f"trying to read existing kontrollutslag using this query: {query}."
-            )
-            df_allerede_kontrollert = self.conn.query(
-                query,
-                self.partitions_skjema,
-            )
-        except Exception as e:  # TODO better exception handling.
-            logger.debug(f"Exception happened:\n{e}")
-            df_allerede_kontrollert = pd.DataFrame(
-                columns=[*self.partitions_skjema.keys(), "kontrollid", "ident", "refnr"]
-            )
-        control_results = self.run_all_controls()
+if __name__ == "__main__":
+    import eimerdb as db
 
-        # Change below condition to 'if df_allerede_kontrollert.empty'?
-        # The below part of logic should only run if there are already rows in the 'kontrollutslag' table.
-        if len(df_allerede_kontrollert) != 0:  # TODO: Separate into its own method?
-            total_merge = control_results.merge(
-                df_allerede_kontrollert,
-                on=[*self.partitions_skjema.keys(), "kontrollid", "ident", "refnr"],
-                how="outer",
-                indicator=True,
-            )
+    conn = db.EimerDBInstance(
+        "ssb-dapla-felles-data-produkt-prod",
+        "produksjonstilskudd_altinn3",
+    )
+    res = conn.query("SELECT * FROM kontaktinfo WHERE aar = '2018'")
+    print(res)
 
-            control_results = total_merge[total_merge["_merge"] == "left_only"][
-                [
-                    *self.partitions_skjema.keys(),
-                    "kontrollid",
-                    "ident",
-                    "refnr",
-                    "verdi",
-                    "utslag_x",
-                ]
-            ].rename(columns={"utslag_x": "utslag"})
+    test = ControlFrameworkBase(
+        time_units=["aar"],
+        applies_to_forms=["RA-7357"],
+        conn=None
+    )
 
-        return control_results
+    test.register_controls()
 
-    def insert_new_rows(self) -> int:
-        """Inserts any new control results that are not already in 'kontrollutslag'.
-
-        Returns:
-            int: Number of rows inserted.
-
-        Raises:
-            AttributeError: If 'conn' does not have 'insert' method.
-        """
-        if not hasattr(self.conn, "insert"):
-            raise AttributeError("'conn' object must have 'insert' method.")
-        df_lastes = self.control_new_rows()
-        logger.debug(f"Data to insert:\n{df_lastes}")
-        if len(df_lastes) > 0:
-            logger.info(f"{len(df_lastes)} nye rader lastes inn...")
-            self.conn.insert("kontrollutslag", df_lastes)
-            logger.info("Innlasting fullført!")
-        else:
-            logger.info("Ingen nye rader å inserte")
-        return len(df_lastes)
+    test.execute_controls()
