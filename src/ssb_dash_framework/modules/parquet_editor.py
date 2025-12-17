127f283aa35e53be689cb5a443c6f74f1c7ab31e
ssb-dor <dor@ssb.no>
2025-12-17 09:33:12 +0100

    Make sure subset argument is treated as list

--- src/ssb_dash_framework/modules/parquet_editor.py
+++ src/ssb_dash_framework/modules/parquet_editor.py
@@ -19,7 +19,6 @@ from dash.dependencies import Input
 from dash.dependencies import Output
 from dash.dependencies import State
 from dash.exceptions import PreventUpdate
-from pandas.io import parquet
 from ssb_poc_statlog_model.change_data_log import ChangeDataLog
 
 from ..setup.variableselector import VariableSelector
@@ -28,13 +27,20 @@ from ..utils.module_validation import module_validator
 
 logger = logging.getLogger(__name__)
 
-def check_for_bucket_path(path):
+
+def check_for_bucket_path(path: str) -> None:
     """Temporary check to make sure users keep to using '/buckets/' paths.
-    
+
     Need to test more with UPath to make sure nothing unexpected happens.
+
+    Raises:
+        NotImplementedError if path doesn't start with '/buckets/'
     """
     if not path.startswith("/buckets/"):
-        raise NotImplementedError("Due to differences in how files in '/buckets/...' behave compared to files in the cloud buckets this functionality is currently limited to only work with paths that starts with '/buckets'.")
+        raise NotImplementedError(
+            "Due to differences in how files in '/buckets/...' behave compared to files in the cloud buckets this functionality is currently limited to only work with paths that starts with '/buckets/'."
+        )
+
 
 class ParquetEditor:  # TODO add validation of dataframe, workshop argument names
     """Simple module with the sole purpose of editing a parquet file.
@@ -669,6 +675,7 @@ def _raise_if_duplicates(df: pd.DataFrame, subset: set[str] | list[str]) -> None
     Raises:
         ValueError: If there are duplicates based on the id_vars specified in the jsonl log.
     """
+    subset = list(subset)
     dupes = df.duplicated(subset=subset, keep=False)
 
     if dupes.any():
