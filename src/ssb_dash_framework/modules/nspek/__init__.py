b0dc85659cf985cd7c67cc908ec78e0e28700d3f
ssb-sik <sik@ssb.no>
2026-05-05 13:12:16 +0200

    added __init__.py

---
+++ src/ssb_dash_framework/modules/nspek/__init__.py
@@ -0,0 +1,5 @@
+from .nspek import Naeringsspesifikasjon
+from .nspek import NaeringsspesifikasjonWindow
+from .nspek import NaeringsspesifikasjonTab
+
+__all__=["Naeringsspesifikasjon", "NaeringsspesifikasjonWindow", "NaeringsspesifikasjonTab"]
