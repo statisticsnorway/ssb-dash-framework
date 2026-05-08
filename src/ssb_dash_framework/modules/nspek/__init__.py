18bafefd727092f472077ef703623a1c7d70dbc5
ssb-sik <sik@ssb.no>
2026-05-08 12:07:41 +0200

    <tekst som beksriver endringer>

--- src/ssb_dash_framework/modules/nspek/__init__.py
+++ src/ssb_dash_framework/modules/nspek/__init__.py
@@ -1,6 +1,9 @@
 from .nspek import Naeringsspesifikasjon
 from .nspek import NaeringsspesifikasjonTab
 from .nspek import NaeringsspesifikasjonWindow
+from .nspek_control_view import NspekControlViewTab
+from .nspek_control_view import NspekControlViewWindow
+from .mock_controls import NspekMockControls
 from .nspek_utils import get_nspek_connection
 from .nspek_utils import set_nspek_connection
 
@@ -8,6 +11,9 @@ __all__ = [
     "Naeringsspesifikasjon",
     "NaeringsspesifikasjonTab",
     "NaeringsspesifikasjonWindow",
+    "NspekControlViewTab",
+    "NspekControlViewWindow",
+    "NspekMockControls",
     "get_nspek_connection",
     "set_nspek_connection",
 ]
