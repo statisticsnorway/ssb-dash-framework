00da6d63cff401aa0ecfc5b391506010d12f747e
Magnus Theodor Engh <dor@ssb.no>
2026-05-29 13:21:14 +0200

    Added include support in yaml reading

--- src/ssb_dash_framework/config/yaml_parser.py
+++ src/ssb_dash_framework/config/yaml_parser.py
@@ -1,8 +1,24 @@
+from pathlib import Path
+
 import yaml
 
-# TODO: Add an !include loader thing
+
+def _build_yaml_loader(base_path: Path) -> type:
+    class CustomLoader(yaml.SafeLoader):
+        pass
+
+    def include_constructor(loader, node):
+        include_path = Path(loader.construct_scalar(node))
+        if not include_path.is_absolute():
+            include_path = base_path / include_path
+        with open(include_path) as f:
+            return yaml.load(f, Loader=_build_yaml_loader(include_path.parent))
+
+    CustomLoader.add_constructor("!include", include_constructor)
+    return CustomLoader
 
 
-def config_parser_yaml(path: str):
+def config_parser_yaml(path: str) -> dict:
+    path = Path(path)
     with open(path) as f:
-        return yaml.safe_load(f)
+        return yaml.load(f, Loader=_build_yaml_loader(path.parent))
