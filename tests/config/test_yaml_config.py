7700224458dfa2fefbe08dabdc39555115deba1b
Magnus Theodor Engh <dor@ssb.no>
2026-05-29 13:44:41 +0200

    Update tests

--- tests/config/test_yaml_config.py
+++ tests/config/test_yaml_config.py
@@ -1,7 +1,9 @@
-from ssb_dash_framework.config.models import AppConfig
-from ssb_dash_framework.config.models import AppModules
-from ssb_dash_framework.config.models import AppSettings
-from ssb_dash_framework.config.models import ModuleConfig
+from ssb_dash_framework import AppConfig
+from ssb_dash_framework import AppModules
+from ssb_dash_framework import AppSettings
+from ssb_dash_framework import ModuleConfig
+from ssb_dash_framework import VariableSelectorConfig
+from ssb_dash_framework import build_app_from_config
 
 
 def test_yaml_app_settings(config_yaml):
@@ -15,9 +17,17 @@ def test_yaml_module_config(config_yaml):
         ModuleConfig(**module)
 
 
+def test_yaml_variableselector_config(config_yaml):
+    VariableSelectorConfig(**config_yaml["app_settings"]["variableselector"])
+
+
 def test_yaml_app_modules(config_yaml):
     AppModules(**config_yaml["modules"])
 
 
 def test_yaml_app_config(config_yaml):
     AppConfig(**config_yaml)
+
+
+def test_build_app_from_config(config_yaml):
+    build_app_from_config(AppConfig(**config_yaml))
