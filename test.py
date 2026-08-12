4d48d30e1146924423aff08342e5aecafef07a39
Magnus Theodor Engh <90612127+MagnusEngh@users.noreply.github.com>
2026-08-12 12:05:47 +0200

    Dataeditor userapi (#415)
    * Example of user facing api

---
+++ test.py
@@ -0,0 +1,2 @@
+import pandas as pd
+empty_df = lambda: pd.DataFrame()
--- tests/module_tests/dataeditor/test_data_views.py
+++
@@ -1,5 +0,0 @@
-# from ssb_dash_framework.experimental.modules.data_editor.data_view.data_view_custom import (
-#     DataViewCustom,
-# )
-
-# DataViewCustom.from_yaml()
