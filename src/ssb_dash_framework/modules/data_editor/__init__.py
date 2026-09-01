80e6cba58e1c5c8d51bcf54bdf667af10f3a7813
ssb-dor <dor@ssb.no>
2026-09-01 09:10:12 +0200

    Fix import issue

--- src/ssb_dash_framework/modules/data_editor/__init__.py
+++ src/ssb_dash_framework/modules/data_editor/__init__.py
@@ -1,14 +1,15 @@
 from .default_getter.getter import StandardDataHandler
 from .editor import DataEditor
-from .modules import DataEditorHistory
-from .modules import DataEditorInfoRow
-from .modules import DataEditorSidebarComment
-from .modules import DataEditorSidebarEditingStatus
-from .modules import DataEditorSupportTable
-from .modules import DataEditorSupportTables
-from .modules import DataEditorTable
-from .modules import DataEditorTableSelector
-from .modules import DataViewCustom
+from .meta import FetcherMeta
+from .modules.data_view.custom_view import DataViewCustom
+from .modules.data_view.table_view import DataEditorTable
+from .modules.helper_buttons.history import DataEditorHistory
+from .modules.helper_buttons.supporting_table import DataEditorSupportTable
+from .modules.helper_buttons.supporting_table import DataEditorSupportTables
+from .modules.inforow.info_row import DataEditorInfoRow
+from .modules.sidebar.editing_comment_view import DataEditorSidebarComment
+from .modules.sidebar.editing_status_view import DataEditorSidebarEditingStatus
+from .modules.sidebar.table_selector import DataEditorTableSelector
 from .utils import EditorSettings
 
 __all__ = [
@@ -23,5 +24,6 @@ __all__ = [
     "DataEditorTableSelector",
     "DataViewCustom",
     "EditorSettings",
+    "FetcherMeta",
     "StandardDataHandler",
 ]
